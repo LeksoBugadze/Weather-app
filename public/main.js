d7a2ac071ffe3b3dc29a82a04d4f066b6bb51950
LeksoBugadze <lesksobugadze@gmail.com>
2025-06-14 23:16:32 +0400

    changed url from http to https

--- public/main.js
+++ public/main.js
@@ -107,7 +107,7 @@ const data=async(city,unit)=>{
     }
     
     output.innerHTML=``;
-    const url=`http://weather-app-server.aleksandrebugadze.com/api?q=${city}&units=${unit}`;
+    const url=`https://weather-app-server.aleksandrebugadze.com/api?q=${city}&units=${unit}`;
 
     
 
