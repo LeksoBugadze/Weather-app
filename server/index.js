26b12a8c4d39599ba2c022aa3fb2fdd92f17a5cc
LeksoBugadze <lesksobugadze@gmail.com>
2025-06-14 23:00:39 +0400

    moved routes to server folder

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
-import {router} from '../routes/index.js'
+import {router} from './routes/index.js'
 import rateLimit from "express-rate-limit"
 
 dotenv.config();
